32f265fb26214bc22a77298a4d06fb2458685784
Cristian Vlaicu <cristian_fly90@yahoo.es>
2024-10-24 15:56:20 +0200

    Send an email with news titles and descriptions.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
     # Vamos a crear un programa que nos va a mandar un email al día con noticias sobre un tema que elijamos. Se usará una API para recolectar esa información.
 
 import requests  # con esta librería vamos a acceder a esa API para recolectar la información.
+from send_email import send_email
 
 apy_key = "0df5ef5a18d64fcd88136b3f2349b948"
 url = "https://newsapi.org/v2/everything?q=apple&" \
@@ -11,6 +12,10 @@ request = requests.get(url)  # requerimos la url con el formato API para que nue
 content = request.json()     # y después de leerlo guardamos el texto en una variable del tipo string. Al poner .json esa variable pasa a ser del tipo diccionario.
 
 # Recorremos de uno en uno los artículos que hay en el diccionario contect antes creado, dentro de la clave articles.
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:  # con el condicional aseguramos que enviar los artículos de verdad, con título y no me mande otra cosa.
+        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+
+body = body.encode('utf-8')
+send_email(message=body)
